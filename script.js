7b8100ceadc67ac8e1df74b13707901672069e15
Karim Yastrov <gkarimv@gmail.com>
2025-02-27 19:09:35 +0500

    #1 Добавление стилей

--- script.js
+++ script.js
@@ -3,16 +3,26 @@ let countAnswer = 0;
 let nextBtn = document.getElementById('nextBtn');
 let options = document.getElementById('options')
 let question = document.getElementById('question');
+let responceDiv = document.getElementById('responce')
+let result = document.getElementById('result')
 
 nextBtn.addEventListener('click', function () {
+    responceDiv.innerHTML = ``
     if (countAsk < data.length) {
         displayAsk(countAsk);
         countAsk++;
     }
     else {
-        options.innerHTML = `
-        <div>Вы закончили игру!</div>
-        <div>Вы заработали ${countAnswer}!</div>
+        question.innerHTML = '';
+        options.innerHTML = '';
+        result.innerHTML = `
+            <p>Ты заработал/а <b>${countAnswer}</b> балла!</p>
+            <br>
+            <p>5 баллов - <b>кент</b></p>
+            <p>4 баллов - <b>ну норм чел</b></p>
+            <p>3 баллов - <b>...</b></p>
+            <p>2 баллов - <b>постыдился бы</b></p>
+            <p>1 баллов - <b>иди извиняться</b></p>
     `
     }
 })
@@ -40,10 +50,12 @@ function displayAsk(countAsk) {
             });
             if (index == currentAnswer) {
                 element.classList.add('correct');
+                getGoodResponce()
                 countAnswer++; 
             }
             else {
                 element.classList.add('wrong');
+                getBadResponce()
             }
         });
         
@@ -51,16 +63,52 @@ function displayAsk(countAsk) {
 }
 
 
+function getBadResponce() {
+    let badWords = ["нихера ты придумал", "ну дебил", "ну ты изюм сушенный", "кент ты сигаретный", "поэтому и иди нахуй", "гори в котлу", "ебушки воробушки", "готовь защеканочку", "п*д*р*с", "кукареку как ты"];
+    let number = Math.floor(Math.random() * 10);
+    let responce = badWords[number];
+    responceDiv.innerHTML = `
+        <p class="responce-item">${responce}</p>
+    `
+}
+function getGoodResponce() {
+    let goodWords = ["поэтому ты и красивый/ая", "нихуя ты знаешь", "храни тебя господь", "ну видно молодец", "ашалеть ты шаришь", "ну и мозги!","возьми печеньку с полки","ну это каждый знает","никому не говори только","бляя стяще",];
+    let number = Math.floor(Math.random() * 10);
+    let responce = goodWords[number];
+    responceDiv.innerHTML = `
+        <p class="responce-item">${responce}</p>
+    `
+}
 
 data = [
     {
-        question: "How many you have?",
-        options: ["первый", "второй", "третий", "четвертый"],
+        question: "Моя любимая марка машины?",
+        options: ["BMW", "Audi", "Mercedes", "Toyota"],
+        answer: 1
+    },
+    {
+        question: "Самое любимое блюдо из перечисленных",
+        options: ["Плов", "Шашлык", "Курица с картошкой", "Роллы"],
         answer: 0
     },
     {
-        question: "How many?",
-        options: ["11", "22", "33", "44"],
+        question: "Ради интереса. Кто король рок н ролла?",
+        options: ["Пол Маккартни", "Курт Кобейн", "Элвис Пресли", "Фредди Меркьюри"],
         answer: 2
+    },
+    {
+        question: "Че самое красивое в этой жизни?",
+        options: ["Меган Фокс", "RS6", "Диана", "Ольга с подьезда"],
+        answer: 1
+    },
+    {
+        question: "А че тогда самое лучшее?",
+        options: ["Жить", "Парить", "Диана", "Зальный тренажер"],
+        answer: 2
+    },
+    {
+        question: "Зачем я все это сделал?",
+        options: ["Ради интереса", "Знания все-таки", "Нехуй делать", "По приколу"],
+        answer: 1
     }
 ]
